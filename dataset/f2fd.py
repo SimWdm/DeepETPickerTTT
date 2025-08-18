5fc054b732f69cfa5d8dc2aae69686bd72ec3191
SimWdm <wiedemann.simon@yahoo.de>
2025-08-18 06:52:20 +0000

    refactor f2fd.py

--- dataset/f2fd.py
+++ dataset/f2fd.py
@@ -1,67 +1,64 @@
 import random
 import torch
+import numpy as np
 
-def get_f2fd_pair(vol, bernoulli_mask_ratio=0.5, phase_inversion_ratio=0.1, min_mask_radius=0.05, max_mask_radius=0.1):
-    """Applies Fourier-based perturbations to create paired noisy patches."""
-    fft_patch = torch.fft.rfftn(vol)
-    fft_patch = torch.fft.fftshift(fft_patch, dim=(-3, -2))
-    
-    _mask = lambda x: mask(
-        x, 
+
+def get_f2fd_pair(vol, bernoulli_mask_ratio=0.5, bernoulli_mask_patch_size=8, phase_inversion_ratio=0.1, min_mask_radius=0.05, max_mask_radius=0.1):
+    vol_fft = np.fft.rfftn(vol)
+    vol_fft = np.fft.fftshift(vol_fft, axes=(-3, -2))
+    vol_1 = _get_f2fd_vol(
+        vol_fft=vol_fft,
         bernoulli_mask_ratio=bernoulli_mask_ratio,
+        bernoulli_mask_patch_size=bernoulli_mask_patch_size,
         phase_inversion_ratio=phase_inversion_ratio,
         min_mask_radius=min_mask_radius,
         max_mask_radius=max_mask_radius
     )
+    vol_2 = _get_f2fd_vol(
+        vol_fft=vol_fft,
+        bernoulli_mask_ratio=bernoulli_mask_ratio,
+        bernoulli_mask_patch_size=bernoulli_mask_patch_size,
+        phase_inversion_ratio=phase_inversion_ratio,
+        min_mask_radius=min_mask_radius,
+        max_mask_radius=max_mask_radius
+    )
+    return vol_1, vol_2
     
-    fft_patch1 = _mask(fft_patch)
-    fft_patch2 = _mask(fft_patch)
 
-    # Inverse FFT
-    ifft_patch1 = torch.fft.irfftn(torch.fft.ifftshift(fft_patch1, dim=(-3, -2)), s=vol.shape)
-    ifft_patch2 = torch.fft.irfftn(torch.fft.ifftshift(fft_patch2, dim=(-3, -2)), s=vol.shape)
-    
-    return ifft_patch1.float(), ifft_patch2.float()
-
-def mask(vol_fft, bernoulli_mask_ratio, phase_inversion_ratio, min_mask_radius, max_mask_radius):
+def _get_f2fd_vol(vol_fft, bernoulli_mask_ratio=0.5, bernoulli_mask_patch_size=8, phase_inversion_ratio=0.1, min_mask_radius=0.05, max_mask_radius=0.1):
     # Patch-based Bernoulli Masking (8x8x8 patches)
     size = vol_fft.shape[0]
-    patch_size = 8
-    mask_shape = (size // patch_size, size // patch_size, 1 + (size // 2 + 1) // patch_size)
-    patch_mask = (torch.rand(*mask_shape, device=vol_fft.device) > bernoulli_mask_ratio).float()
-    
-    # Expand mask to match fft_patch shape
-    # bernoulli_mask = patch_mask.numpy().repeat(patch_size, axis=0)
-    # bernoulli_mask = bernoulli_mask.repeat(patch_size, axis=1)
-    # bernoulli_mask = bernoulli_mask.repeat(patch_size, axis=2)
-    bernoulli_mask = patch_mask.repeat((patch_size, 1, 1))
-    bernoulli_mask = bernoulli_mask.repeat(1, patch_size, 1)
-    bernoulli_mask = bernoulli_mask.repeat(1, 1, patch_size)
-    bernoulli_mask = bernoulli_mask[:size, :size, :size // 2 + 1]  # Ensure matching shape
-    
-    # fft_masked = fft_patch * bernoulli_mask
-    
+    mask_shape = (size // bernoulli_mask_patch_size, size // bernoulli_mask_patch_size, 1 + (size // 2 + 1) // bernoulli_mask_patch_size)
+    patch_mask = (np.random.rand(*mask_shape) > bernoulli_mask_ratio).astype(np.float32)
+    # Expand mask to match vol_fft shape
+    bernoulli_mask = patch_mask.repeat(bernoulli_mask_patch_size, axis=0)
+    bernoulli_mask = bernoulli_mask.repeat(bernoulli_mask_patch_size, axis=1)
+    bernoulli_mask = bernoulli_mask.repeat(bernoulli_mask_patch_size, axis=2)
+    bernoulli_mask_1 = bernoulli_mask[:size, :size, :size // 2 + 1]  # Ensure matching shape
+    #bernoulli_mask_2 = 1 - bernoulli_mask_1  # Inverse mask for the second volume
+        
     # Random Phase Inversion
-    phase_flip = (torch.rand(*vol_fft.real.shape, device=vol_fft.device) < phase_inversion_ratio).float()
-    fft_patch_inverted = vol_fft * ((-1) ** phase_flip)
+    phase_flip = (np.random.rand(*vol_fft.real.shape) < phase_inversion_ratio).astype(np.float32)
+    vol_fft_inverted = vol_fft * ((-1) ** phase_flip)
     
     # Spherical Mask to keep low frequencies
     center = size // 2
-    # radius = random.randint(int(0.05 * size), int(0.1 * size))
     radius = random.randint(int(min_mask_radius * size), int(max_mask_radius * size))
-    x, y, z = torch.meshgrid(
-        torch.arange(size, device=vol_fft.device), 
-        torch.arange(size, device=vol_fft.device), 
-        torch.arange(size // 2 + 1, device=vol_fft.device), 
-        indexing='ij'
-    )
+    x, y, z = np.meshgrid(torch.arange(size), torch.arange(size), torch.arange(size // 2 + 1), indexing='ij')
     mask = ((x - center) ** 2 + (y - center) ** 2 + (z) ** 2) < (radius ** 2)
+    mask = mask.astype(np.float32)
     
-    mask = mask.float()
+    # Combine masks
+    overall_mask_1 = np.logical_or(mask, bernoulli_mask_1).astype(np.float32)
+    #overall_mask_2 = np.logical_or(mask, bernoulli_mask_2).astype(np.float32)
 
-    overall_mask = torch.logical_or(mask, bernoulli_mask)
-    # fft_masked = torch.where(mask, fft_masked, torch.zeros_like(fft_masked))
-    return fft_patch_inverted * overall_mask.float()
+    vol_1_fft = vol_fft_inverted * overall_mask_1
+    #vol_2_fft = vol_fft_inverted * overall_mask_2
+    
+    vol_1 = np.fft.irfftn(np.fft.ifftshift(vol_1_fft, axes=(-3, -2)), s=vol.shape)
+    #vol_2 = np.fft.irfftn(np.fft.ifftshift(vol_2_fft, axes=(-3, -2)), s=vol.shape)    
+        
+    return vol_1 #, vol_2
 
 
 
