59086a68ad2aadd8156145566d877e740fcb0b76
SimWdm <wiedemann.simon@yahoo.de>
2026-01-29 13:03:50 +0000

    add .sh scripts to backup

--- utils/backup.py
+++ utils/backup.py
@@ -2,11 +2,16 @@ import glob
 import os
 import shutil
 
-def backup_python_files(src, dest, exclude_dirs=[]):
+def backup_python_files(src, dest, exclude_dirs=[], extensions=[".py", ".sh"]):
     """
     Recursively copies all Python files in src to dest. If dest or any subdirectory does not exist, they are created. This has saved my life quite a few times.
     """
-    for file_path in glob.glob(os.path.join(src, '**', '*.py'), recursive=True):
+    # find all files with the given extensions
+    file_paths = []
+    for ext in extensions:
+        file_paths.extend(glob.glob(f"{src}/**/*{ext}", recursive=True))
+    for file_path in file_paths:
+        # skip backup files and excluded dirs
         if "/code_backup/" in file_path:
             continue
         if any([file_path.startswith(dir) for dir in exclude_dirs]):
